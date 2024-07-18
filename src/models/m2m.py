48d0f0041bb9b622c0986d0e74c2ff86e7db43c6
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-07-19 00:09:02 +0300

    added UniqueConstraint in m2m model

--- src/models/m2m.py
+++ src/models/m2m.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from databases.database import Base
@@ -14,6 +14,9 @@ class UsersCities(Base):
     """
 
     __tablename__ = "users_cities"
+    __table_args__ = (
+        UniqueConstraint("city_id", "user_id", name="uix_city_user"),
+    )
     city_id: Mapped[int] = mapped_column(
         ForeignKey("city.id", ondelete="CASCADE"),
         primary_key=True,
