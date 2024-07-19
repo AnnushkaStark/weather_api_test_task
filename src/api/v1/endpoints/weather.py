27f0c03264395d4202d27a32cb1262673489af89
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-07-19 04:54:26 +0300

    added weather router

--- src/api/v1/endpoints/weather.py
+++ src/api/v1/endpoints/weather.py
@@ -3,40 +3,46 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from api.dependencies.database import get_async_db
 from api.dependencies.auth import get_current_user
+from api.dependencies.database import get_async_db
 from crud.city import crud_city
 from models.user import User
-from services import city as  service_city
-from schemas.city  import CityResponse, CityCreate
+from schemas.city import CityCreate, CityResponse
+from services import city as service_city
 from utilies.weather.get_weather import request_weather
-
 
 router = APIRouter()
 
 
-@router.post("/",status_code=status.HTTP_200_OK)
+@router.post("/", status_code=status.HTTP_200_OK)
 async def get_weather_in_city(
     city: CityCreate,
     db: Session = Depends(get_async_db),
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_user),
 ) -> Optional[dict]:
     city.name = city.name.strip().title()
     if found_city := await crud_city.get_by_name(db=db, obj_name=city.name):
         return await request_weather(city=found_city.name)
     weather = await request_weather(city=city.name)
     if weather is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Wrong city name")
-    await service_city.create(db=db, create_data=city, user_id=current_user.id)
-        #return weather
-    #except Exception as e:
-    return weather
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Wrong city name"
+        )
+    try:
+        await service_city.create(
+            db=db, create_data=city, user_id=current_user.id
+        )
+        return weather
+    except Exception as e:
+        return weather
 
 
 @router.get("/", response_model=List[CityResponse])
 async def read_search_history(
     db: Session = Depends(get_async_db),
-    current_user: User = Depends(get_current_user)
-): 
-    found_cities = await crud_city.get_multi_by_user_id(db=db, user_id=current_user.id)
+    current_user: User = Depends(get_current_user),
+):
+    found_cities = await crud_city.get_multi_by_user_id(
+        db=db, user_id=current_user.id
+    )
     return found_cities
--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from api.v1.endpoints.user import router as user_router
+from api.v1.endpoints.weather import router as weather_router
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(user_router, prefix="/users", tags=["Users"])
+api_router.include_router(weather_router, prefix="/weather", tags=["Weather"])
